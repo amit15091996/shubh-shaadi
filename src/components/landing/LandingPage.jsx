fc765a2ba0b3b52af9f9a720d7aafaeb363dde87
fhdewangan1 <fanishdewanganrock@gmail.com>
2024-11-17 03:32:09 -0800

    fixed import

--- src/components/landing/LandingPage.jsx
+++ src/components/landing/LandingPage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./LandingPage.css";
+import "../landing/LandingPage";
 
 const LandingPage = () => {
   const [gender, setGender] = useState("");
