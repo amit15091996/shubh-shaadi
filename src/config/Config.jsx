67707103cc930320412f717585810fa5ab968a2e
fhdewangan1 <fanishdewanganrock@gmail.com>
2024-12-02 06:13:12 -0800

    fixed

--- src/config/Config.jsx
+++ src/config/Config.jsx
@@ -2,6 +2,6 @@
 // export const ApiUrl = "http://localhost:7878/api/v1";
 
 // Production Environment
-//export const ApiUrl = "https://shaadi-be.fino-web-app.agency/api/v1";
+export const ApiUrl = "https://shaadi-be.fino-web-app.agency/api/v1";
 
 // NOTE : don't use Config.jsx for api call use AxiosConfig instead
